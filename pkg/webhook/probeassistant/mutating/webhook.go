48ca2002ff5c58527bf7d1ec127737ee2a8b44d6
twinkle <32890238+twink7e@users.noreply.github.com>
2021-08-03 18:13:05 +0800

    删除：     pkg/webhook/probeassistant/mutating/webhook.go 	删除：     pkg/webhook/probeassistant/validating/webhook.go

--- pkg/webhook/probeassistant/mutating/webhook.go
+++
@@ -1,9 +0,0 @@
-package mutating
-
-import "sigs.k8s.io/controller-runtime/pkg/webhook/admission"
-
-// SidecarSetCreateHandler handles SidecarSet
-type ProbeAssistantCreateHandler struct {
-	// Decoder decodes objects
-	Decoder *admission.Decoder
-}
